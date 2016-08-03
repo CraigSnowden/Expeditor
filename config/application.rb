a68b67a72ddcdee0f6a175030712dc46b10726cc
Craig Snowden <craig@craigsnowden.com>
2016-08-03 17:25:37 +0100

    Configure for cable

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Expeditor
   class Application < Rails::Application
+    config.action_cable.mount_path = '/cable'
+    config.action_cable.disable_request_forgery_protection = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
